e7d5d989e87ebfc4ab9d52d46e7bace0bfb42931
Matt Hughson <matt.hughson@gmail.com>
2022-07-04 21:01:11 -0700

    Updated CBTFX to latest. NOTE: This requires a re-export of sound files in game.

--- common/src/cbtfx.c
+++ common/src/cbtfx.c
@@ -12,16 +12,15 @@ Also thanks to bbbbbr for getting my code to ACTUALLY work
 #pragma bank 5
 #include <gb/gb.h>
 #include "cbtfx.h"
-#include "Music.h"
-
-// Don't forget to include your music driver here if you're gonna use it!
-// For hUGEdriver
+#include "hUGEDriver.h"
 #define MUSIC_DRIVER_CH2_ON hUGE_mute_channel(HT_CH2, 0);
 #define MUSIC_DRIVER_CH2_OFF hUGE_mute_channel(HT_CH2, 1);
 #define MUSIC_DRIVER_CH4_ON hUGE_mute_channel(HT_CH4, 0);
 #define MUSIC_DRIVER_CH4_OFF hUGE_mute_channel(HT_CH4, 1);
+// 0 = Panning won't be reset after an SFX, 1 = Panning will be set to 0XFF after an SFX plays.
+#define MONO_MUSIC 0
 
-const unsigned char CBTFX_HEADER[] = "CBT-FX BY COFFEEBAT 2021";
+const unsigned char CBTFX_HEADER[] = "CBT-FX BY COFFEEBAT 2021-2022";
 const uint8_t * CBTFX_pointer;
 uint8_t CBTFX_size = 0;
 uint8_t CBTFX_repeater = 0;
@@ -30,62 +29,77 @@ uint8_t CBTFX_priority = 0;
 uint8_t CBTFX_ch_used = 0;
 
 //Restart values and point to the new sfx
-void CBTFX_init(const unsigned char * SFX, uint8_t length, uint8_t priority, uint8_t ch_used) BANKED {
-    if (priority < CBTFX_priority) return;
-    CBTFX_priority = priority;
-    CBTFX_ch_used = ch_used;
-    CBTFX_pointer = SFX;
-    CBTFX_size = length;
+void CBTFX_init(const unsigned char * SFX) BANKED {
+    if ((*SFX & 0x0f) < CBTFX_priority) return;
+    // To avoid hanging notes
+    if (CBTFX_ch_used & 128) NR21_REG = NR22_REG = NR23_REG = NR24_REG = 0;
+    if (CBTFX_ch_used & 32) NR41_REG = NR42_REG = NR43_REG = NR44_REG = 0;
+    // To avoid the priority system leaving some channels turned off (Don't ask how I discovered this...)
+    MUSIC_DRIVER_CH2_ON;
+    MUSIC_DRIVER_CH4_ON;
+    CBTFX_priority = *SFX & 0x0f;
+    CBTFX_ch_used = *SFX++;
+    CBTFX_size = *SFX++;
     CBTFX_repeater = 0;
-    CBTFX_panning = NR51_REG;
-    MUSIC_DRIVER_CH2_OFF
-    MUSIC_DRIVER_CH4_OFF
+    CBTFX_pointer = SFX;
+    if (CBTFX_ch_used & 128) MUSIC_DRIVER_CH2_OFF;
+    if (CBTFX_ch_used & 32) MUSIC_DRIVER_CH4_OFF;
 }
 
 void CBTFX_update(void) BANKED {
-    if (CBTFX_size != 0){
+    if (CBTFX_size != 0){ // If we have an SFX to play...
 
-        NR51_REG = CBTFX_panning;
-
-    	if(CBTFX_repeater != 0){
-    		CBTFX_repeater--;
+    	if(CBTFX_repeater != 0){ // If we are still playing a frame
+    		CBTFX_repeater--; // Remove one from the frame counter
     	}else{
 
-        	CBTFX_repeater = *CBTFX_pointer++;
-
-            NR51_REG |= *CBTFX_pointer++;
-
-            if (CBTFX_ch_used & 0xF0){
+        	CBTFX_repeater = *CBTFX_pointer++; // Load the frame's length
+			
+	        uint8_t mask = 0; // Mask to avoid muting an unused channel
+	        if (CBTFX_ch_used & 128) mask |= 0x22;
+	        if (CBTFX_ch_used & 32) mask |= 0x88;
+	        NR51_REG &= ~mask; // Mask out the CH2 and CH4 pan values
+	        NR51_REG |= mask & *CBTFX_pointer++; // And write ours
+            
+            if (CBTFX_ch_used & 128){
                 NR21_REG = *CBTFX_pointer++;
-                NR22_REG = *CBTFX_pointer & 0xF0;
-            }else CBTFX_pointer++;
-
-            if (CBTFX_ch_used & 0x0F){
-                NR42_REG = (*CBTFX_pointer & 0x0F) << 4;
+                NR22_REG = *CBTFX_pointer & 0xf0; // To assure the envelope is set to 0
+            }
+            
+            if (CBTFX_ch_used & 32){
+                NR42_REG = *CBTFX_pointer << 4; // Volume for the noise channel is the lower 4 bits of the same byte
             }
 
             CBTFX_pointer++;
-            if (CBTFX_ch_used & 0xF0){
-                NR23_REG = *CBTFX_pointer++;
-                NR24_REG = *CBTFX_pointer++;
-            }else CBTFX_pointer += 2;
 
-            if (CBTFX_ch_used & 0x0F){
-                NR41_REG = *CBTFX_pointer++;
-                NR43_REG = *CBTFX_pointer++;
-                NR44_REG = *CBTFX_pointer++;
-            }else CBTFX_pointer += 3;
+            // If CH2 isn't used, we omit this data
+            if (CBTFX_ch_used & 128) {
+                NR23_REG = *CBTFX_pointer++; // The lower 8 bits of the frequency
+                NR24_REG = *CBTFX_pointer++; // Higher 3 bits of the frequency + making sure the length isn't used and triggering the channel
+            }
+
+            // If CH4 isn't used, we omit this data
+            if (CBTFX_ch_used & 32) {
+                NR43_REG = *CBTFX_pointer++; // Noise freq
+                NR44_REG = 0x80; // Trigger the noise channel
+            }
 
             CBTFX_size--;
 
-            CBTFX_panning = NR51_REG;
-
             if(CBTFX_size == 0){
-                CBTFX_priority = NR21_REG = NR22_REG = NR23_REG = NR24_REG = NR41_REG = NR42_REG = NR43_REG = NR44_REG = 0;
-                NR51_REG |= 0b10101010;
-                MUSIC_DRIVER_CH2_ON
-                MUSIC_DRIVER_CH4_ON
-            }          
+           		CBTFX_priority = 0;
+                MUSIC_DRIVER_CH2_ON;
+                MUSIC_DRIVER_CH4_ON;
+                if (CBTFX_ch_used & 128){
+                    NR21_REG = NR22_REG = NR23_REG = NR24_REG = 0;
+                }
+                if (CBTFX_ch_used & 32){
+                    NR41_REG = NR42_REG = NR43_REG = NR44_REG = 0;
+                }
+                #if (MONO_MUSIC==1)  
+					NR51_REG = 0xff;
+				#endif
+            }
 
         }
     }
